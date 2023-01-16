ec30930fa8d47514011a3eeabbe74d1e83a845fb
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-16 23:21:29 +0100

    Minor edit for scrolling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,9 +21,9 @@ const Navbar = () => {
                 {socialData.map(item=><li key={item.id}><a href={item.href} ><item.icon/></a></li>)}
             </ul>
             <FaBars onClick={_=>setShowMenu(p=>!p)} className={`block md:hidden text-gray-600 text-3xl z-30 cursor-pointer transition duration-500 ${showMenu?'rotate-90':'rotate-0'}`}/>
-            <div className={`fixed top-[120px] ${showMenu?'translate-y-0':'translate-y-[-130%]'} transition duration-500 left-0 w-full h-auto bg-white`}>
+            <div className={`fixed top-[100px] ${showMenu?'translate-y-0':'translate-y-[-130%]'} transition duration-500 left-0 w-full h-auto bg-white border-b-gray-600 border-b-2 shadow shadow-gray-600`}>
                 <ul className='flex flex-col my-10 items-center gap-4 text-gray-600 font-semibold text-lg '>
-                    {navData.map(item=><li key={item.id} className='hover:border-gray-600 hover:border-b-2 cursor-pointer transition duration-500'><Link to={item.name} smooth={true}>{item.name}</Link></li>)}
+                    {navData.map(item=><li key={item.id}  className='hover:border-gray-600 border-b-2 border-white cursor-pointer transition duration-500'><Link to={item.name} smooth={true}><p onClick={_=>setShowMenu(false)}>{item.name}</p></Link></li>)}
                 </ul>
                 <ul className='flex justify-center my-10 items-center gap-4 text-xl text-gray-600'>
                     {socialData.map(item=><li key={item.id}><a href={item.href} ><item.icon/></a></li>)}
