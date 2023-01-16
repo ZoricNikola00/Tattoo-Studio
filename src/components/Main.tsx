a5de623523d264fc63ff67defda71745eb6a02e3
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-16 23:34:26 +0100

    Minor edit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,15 +7,15 @@ const Main = () => {
     <section id='Home' className='w-full h-[100vh] mt-[80px] bg-hero relative bg-cover bg-center'>
         <img src={mainText} className='absolute w-full bottom-0'/>
         <motion.div 
-            className='w-[80%] h-full mx-auto flex md:justify-center md:pl-[100px] items-center'
+            className='w-[80%] h-full mx-auto flex justify-center md:pl-[100px] items-center  mb-[100px]'
             initial={{y:-100,opacity:0}}
             whileInView={{y:0,opacity:100}}
             transition={{duration:1}}
         >
-            <div className='text-white font-primary'>
+            <div className='text-white font-primary text-center md:text-left'>
                 <h1 className='text-6xl'>I'm Void Sir.</h1>
-                <p className='text-xl'>'Tattoos have their own unique power and magic.<br className='my-4'/> They not only beautify the body but also the psyche.'</p>
-                <button className='flex items-center font-secondary border-2 border-white rounded-lg p-3 mt-4 hover:bg-white hover:text-black transition duration-500 text-lg'>Read More <IoMdArrowForward className='text-3xl ml-2'/></button>
+                <p className='text-xl'>'Tattoos have their own unique power and magic.<br className='my-4 hidden md:block'/> They not only beautify the body but also the psyche.'</p>
+                <button className='flex items-center font-secondary border-2 mx-auto md:mx-0 border-white rounded-lg p-3 mt-4 hover:bg-white hover:text-black transition duration-500 text-lg'>Read More <IoMdArrowForward className='text-3xl ml-2'/></button>
             </div>
         </motion.div>
 
