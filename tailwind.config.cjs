ec83a5b4c6362f1a6d171efcb8b34797fbc76806
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-16 21:25:14 +0100

    Edit tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      primary: 'Oswald',
+      secondary: 'Raleway',
+      tertiary: 'Rozha One',
+    },
+    backgroundImage: {
+      hero: "url('/src/img/hero/bg.jpg')",
+      about: "url('/src/img/about/image.png')",
+      interview: "url('/src/img/interview/bg.png')",
+    },
     extend: {},
   },
   plugins: [],
