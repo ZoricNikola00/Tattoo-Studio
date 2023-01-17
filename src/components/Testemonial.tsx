d61a9901054cba16f6995f13513cab3b21d88b32
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-17 18:11:26 +0100

    Minor edit

--- src/components/Testemonial.tsx
+++ src/components/Testemonial.tsx
@@ -12,7 +12,7 @@ const Testemonial = () => {
         transition={{duration:1}}
         className='w-[80%] my-[100px] mx-auto flex justify-center items-center'
     >
-        <div className='relative w-[500px] h-[300px] overflow-hidden'>
+        <div className='relative w-full h-[400px] md:w-[500px] overflow-hidden'>
           <div className='absolute flex items-center top-[10%] right-10 text-gray-600 gap-5 text-3xl'>
             <FaArrowAltCircleLeft onClick={_=>setIndex(p=>p===0?testimonialData.length-1:p-1)} className='cursor-pointer z-30'/>
             <FaArrowAltCircleRight onClick={_=>setIndex(p=>p===testimonialData.length-1?0:p+1)} className='cursor-pointer z-30'/>
@@ -22,7 +22,7 @@ const Testemonial = () => {
             if(index===i) position='translate-x-0 opacity-100'
             if(index-1===i || (index===0 && i===arr.length-1)) position='translate-x-[-100%] opacity-0'
             return <div key={item.id} className={`w-full h-full absolute top-0 left-0 transition duration-500 font-primary ${position}`}>
-              <img src={item.quoteImg} className='absolute top-[10%] left-[0]'/>
+              <img src={item.quoteImg} className='absolute top-[7%] left-[0]'/>
               <p className='text-lg text-gray-800 mt-[150px]'>{item.message}</p>
               <div className='flex items-end gap-4 mt-[10px]'>
                 <p className='text-lg font-bold leading-none'>{item.name}</p>
